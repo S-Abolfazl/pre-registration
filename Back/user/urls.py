4ffc7207fe9a2a017b2b7b3c67075866b33615fe
pouria_02 <p.adabi1381@gmail.com>
2024-11-08 16:01:12 +0330

    gitignore added

--- Back/user/urls.py
+++ Back/user/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('create/', UserCreateApi.as_view(), name='create_user'),
     path('list/', UserListApi.as_view(), name='list_user'),
     path('list/<pk>', UserListApi.as_view(), name='user_derails'),
+    path('update/<pk>', UserUpdateApi.as_view(), name='update_user'),
 ]
 
