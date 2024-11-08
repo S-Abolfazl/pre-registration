5683c05b207a2a799ce28e8c682efeba27c75683
hgor1382 <hgor1382@gmail.com>
2024-11-08 20:07:22 +0330

    login api created

--- Back/user/urls.py
+++ Back/user/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('list/', UserListApi.as_view(), name='list_user'),
     path('list/<pk>', UserListApi.as_view(), name='user_derails'),
     path('signup/', UserSignupApi.as_view(), name='signup_user'),
+    path('login/', UserLoginApi.as_view(), name='login_user'),
 ]
 
--- Back/user/views.py
+++ Back/user/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
 
 from .models import User
 from .serializers import UserSerializer
@@ -14,9 +17,18 @@ class UserSignupApi(APIView):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
+            
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+            
             return Response(data={
                 "msg":"ok",
-                "data":f'user by id:{user.id} created'
+                "data":{
+                    "user": serializer.data,
+                    "access_token": access_token,
+                    "refresh_token": refresh_token
+                }
             }, status=status.HTTP_201_CREATED)
         
         return Response(
@@ -28,7 +40,7 @@ class UserSignupApi(APIView):
         )
 
 class UserListApi(APIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     def get(self, request, pk=None):
         try:
             if pk:
@@ -44,4 +56,31 @@ class UserListApi(APIView):
                 "data":"user not found"
             }, status=status.HTTP_404_NOT_FOUND)
 
-    
+class UserLoginApi(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+            
+            return Response(data={
+                "msg": "ok",
+                "data": {
+                    "user": UserSerializer(user).data,
+                    "access_token": access_token,
+                    "refresh_token": refresh_token
+                }
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response(data={
+                "msg": "error",
+                "data": "Invalid username or password"
+            }, status=status.HTTP_401_UNAUTHORIZED)
