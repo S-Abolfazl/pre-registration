2e8a1169143d5cacceec06a6807e7dfa772b475e
pouria_02 <p.adabi1381@gmail.com>
2024-11-10 13:40:55 +0330

    define courseCreateApi in view and add url in feature/course/create

--- Back/course/views.py
+++ Back/course/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import User
+from .serializers import CourseSerializer
+
+from django.views.decorators.csrf import csrf_exempt
+
+class CourseCreateApi(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            course = serializer.save()
+            return Response(data={
+                "msg":"ok",
+                "data":f'course by id:{course.id} created'
+            }, status=status.HTTP_201_CREATED)
+        
+        return Response(
+            data={
+                "msg":"error",
+                "data": serializer.errors
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
