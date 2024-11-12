61087129d18c3b882e97d161c53f7363ffc60e0e
hgor1382 <hgor1382@gmail.com>
2024-11-12 16:14:07 +0330

    student create api is now available

--- Back/student/serializers.py
+++ Back/student/serializers.py
@@ -5,8 +5,19 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
-        
+    
     def create(self, validated_data):
+        m2m_fields = {}
+        for field in self.Meta.model._meta.many_to_many:
+            if field.name in validated_data:
+                m2m_fields[field.name] = validated_data.pop(field.name)
+        
         validated_data['type'] = 'student'
+        
         student = Student.objects.create(**validated_data)
-        return student 
+        
+        for field_name, value in m2m_fields.items():
+            getattr(student, field_name).set(value)
+        
+        return student
+    
