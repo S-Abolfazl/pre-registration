4b8d36967b50086438f1b29d86863fe859711c32
pouria_02 <p.adabi1381@gmail.com>
2024-11-12 11:47:51 +0330

    feature/course/update has been defined in course.urls

--- Back/course/urls.py
+++ Back/course/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('create/', CourseCreateApi.as_view(), name='create_course'),
     path('list/', CourseListApi.as_view(), name='list_course'),
     path('list/<pk>', CourseListApi.as_view(), name='course_details'),
+    path('update/<pk>', CourseUpdateApi.as_view(), name='update_course'),
     #path('list/<pk>', UserListApi.as_view(), name='user_details'),
 ]
