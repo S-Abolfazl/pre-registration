00a8c987374661406ed0010ebe571572609f7c79
hgor1382 <hgor1382@gmail.com>
2024-11-15 17:26:59 +0330

    hash password and user is_active is now handle in user serilizer

--- Back/user/serializers.py
+++ Back/user/serializers.py
@@ -6,3 +6,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().create(validated_data)
+        
+        if password:
+            user.set_password(password)
+        user.is_active = True
+        user.save()
+        return user
