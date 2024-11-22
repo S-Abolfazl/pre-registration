bdfdbfbb9c0b2ace264ee8a498c06f572402b84d
hgor1382 <hgor1382@gmail.com>
2024-11-22 16:51:42 +0330

    password validation implemented in userserializer

--- Back/user/serializers.py
+++ Back/user/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+import re
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
@@ -29,5 +29,27 @@ class UserSerializer(serializers.ModelSerializer):
         
         if not value.isdigit():
             raise serializers.ValidationError("Usernames must be integers")
+        
+        return value
+    
+    def validate_password(self, value):
+        
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        
+        if not re.search(r'[A-Z]', value):
+            raise serializers.ValidationError("Password must include at least one uppercase letter.")
+        
+        if not re.search(r'[a-z]', value):
+            raise serializers.ValidationError("Password must include at least one lowercase letter.")
+        
+        if not re.search(r'[0-9]', value):
+            raise serializers.ValidationError("Password must include at least one digit.")
+        
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise serializers.ValidationError("Password must include at least one special character (!@#$%^&*(),.?\":{}|<>).")
+        
+        if re.search(r'\s', value):
+            raise serializers.ValidationError("Password must not contain any spaces.")
         
         return value
