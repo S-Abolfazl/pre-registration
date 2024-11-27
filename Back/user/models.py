b2ae8e0bb0dbd1a1205106e2d08e82a09adba98e
hgor1382 <hgor1382@gmail.com>
2024-11-27 18:59:50 +0330

    educational assistant model deleted

--- Back/user/models.py
+++ Back/user/models.py
@@ -23,12 +23,3 @@ class User(AbstractUser):
     
     def __str__(self) -> str:
         return self.username
-    
-
-class EducationalAssisstant(User):
-
-    class Meta:
-        db_table = "educational_assessment"
-
-    def __str__(self) -> str:
-        return self.usernamede
