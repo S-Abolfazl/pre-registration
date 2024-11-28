f561ef297a5e8a97f781202e5349c11babcc5f4a
hgor1382 <hgor1382@gmail.com>
2024-11-28 18:55:56 +0330

    IsAcademicAssistant permition implemented

--- Back/user/permissions.py
+++ Back/user/permissions.py
@@ -12,3 +12,14 @@ class IsStudent(BasePermissions):
             is_student = False
         
         return is_student
+    
+class IsAcademicAssistant(BasePermissions):
+    def has_permission(self, request, view):
+        
+        is_academic_assistant = False
+        try:
+            user = User.objects.get(id=request.user.id)
+            is_academic_assistant = user.type == 'academicassistant'
+        except User.DoesNotExist:
+            is_academic_assistant = False
+        
