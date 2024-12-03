a1876aee7d7c94bad077b654aa13877c4072640d
hgor1382 <hgor1382@gmail.com>
2024-12-03 14:39:03 +0330

    swagger added to project and we can test signup api with that we should make it ok for all endpoints

--- Back/Back/urls.py
+++ Back/Back/urls.py
@@ -16,6 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1",
+        description="Detailed documentation for all API endpoints.",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="support@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +40,7 @@ urlpatterns = [
     path('student/', include('student.urls')),
     path('support/', include('support.urls')),
     path('registration-form/', include('registrationForm.urls')),
+        
+    # swagger
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
