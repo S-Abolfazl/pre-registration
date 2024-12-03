39c3d926e058cd4fd466f7eadab44efe255e796a
hgor1382 <hgor1382@gmail.com>
2024-12-03 12:12:02 +0330

    chart create api is ok and tested

--- Back/student/urls.py
+++ Back/student/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     # path('create/', StudentCreateApi.as_view(), name='create_student'),
-    path('chart/get/', EducationalChartGetApi.as_view(), name='get_student'),
+    path('chart/create/', EducationalChartCreateApi.as_view(), name='create_chart'),
+    path('chart/get/', EducationalChartGetApi.as_view(), name='get_chart'),
 ]
