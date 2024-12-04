e5220894b826698341df04e6e3788a7d8178e147
Mobinhazar <mobinhazarkhani@gmail.com>
2024-12-04 11:54:02 +0330

    pishsabt card fix size and font

--- Front/components/Panel/pishsabt.vue
+++ Front/components/Panel/pishsabt.vue
@@ -4,8 +4,10 @@
       <v-row no-gutters align="center">
         <!-- متن توضیحات -->
         <v-col cols="6" >
-          <div class="font_35 font_bold titlee" >پیش ثبت نام</div>
-          <div class="font_16 description" >
+          <div class="title" >
+            <b class="font_44">پیش ثبت نام</b>
+          </div>
+          <div class="font_20 description" >
             شما اینجا میتوانید به صورت آزمایشی انتخاب واحد کنید تا دانشگاه بهتر بتونه برنامه و ظرفیت کلاس ها رو مشخص کنه.
           </div>
         </v-col>
@@ -27,7 +29,7 @@
   box-shadow: 0px 0px 20px 3px rgba(0, 0, 0, 0.3) !important;
   border-radius: 25px;
   background-color: #7B5FF1;
-  width: 28%; 
+  width: 30%; 
   height: 250px; 
   margin: auto;
   position: relative; /* برای کنترل موقعیت فرزندان مطلق */
@@ -36,24 +38,24 @@
 
 .image-wrapper {
   position: absolute;
-  top: -5%; /* تصویر را از بالا بیرون بیاورید */
-  right: 1%; /* تصویر را از سمت راست کمی خارج کنید */
+  top: -9%; /* تصویر را از بالا بیرون بیاورید */
+  right: 3%; /* تصویر را از سمت راست کمی خارج کنید */
   width: 100%;
   display: flex;
   justify-content: flex-end;
 }
 
 .rocket-image {
-  width: 52%; /* تنظیم سایز تصویر */
+  width: 50%; /* تنظیم سایز تصویر */
   /* z-index: 2; تصویر بالاتر از کارت قرار بگیرد */
 }
-.titlee{
-  margin: 8px 8px auto auto;
+.title{
+  margin: 16px 14px auto auto;
   /* z-index: 2; تصویر بالاتر از کارت قرار بگیرد */
 }
 .description {
-  margin: 8px 8px auto auto;
-  width: 85%; /* تنظیم سایز تصویر */
+  margin: 20px 14px auto auto;
+  width: 100%; /* تنظیم سایز تصویر */
   /* z-index: 2; تصویر بالاتر از کارت قرار بگیرد */
 }
 
