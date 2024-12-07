b1412ae94a05da3e3eaac0714fc6b878b4d32ad0
parmissud <101392326+parmissud@users.noreply.github.com>
2024-12-07 15:31:54 +0330

    Add files via upload
    Navigation bar added to panel components

---
+++ Front/components/Panel/Bar.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-layout>
+    <v-app-bar
+      scroll-behavior="collapse"
+      color="white1"
+      width="95%"
+      rounded="pill"
+    >
+      <template v-slot:prepend>
+        <v-list-item
+          lines="two"
+          :avatar="user.avatar"
+          :subtitle="user.role"
+          :title="user.username"
+        >
+        </v-list-item>
+      </template>
+      <v-spacer></v-spacer>
+      <BaseButton
+        text="پيش ثبت نام"
+        color="primary"
+        width="10%"
+        @click="navigateToPreRegistration"
+      ></BaseButton>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="navigateToFeedback">
+  <v-icon>mdi-forum</v-icon>
+  <span>نظرات</span>
+</v-btn>
+<v-spacer></v-spacer>
+<v-btn icon @click="navigateToNotifications">
+  <v-icon>mdi-bell</v-icon>
+  <span>اعلانات</span>
+</v-btn>
+<v-spacer></v-spacer>
+<v-btn icon @click="navigateToSupport">
+  <v-icon>mdi-headset</v-icon>
+  <span style="position: relative;">پشتیبانی</span>
+</v-btn>
+      <v-spacer></v-spacer>
+      <img :src="require('@/static/image/navbar/sbulogo.png')" alt="sbulogo"  width="4%" height="90%" style=" padding-left: 0.5%">
+    </v-app-bar>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import BaseButton from '../Base/BaseButton.vue';
+
+export default {
+  data: () => ({
+    user: {
+      username: 'حسين گرزين',
+      avatar: '', // Add a valid avatar URL or leave empty for the fallback image
+      role: 'ادمين',
+    },
+  }),
+  methods: {
+    navigateToPreRegistration() {
+      this.$router.push('/pre-registration'); // Adjust the route path as needed
+    },
+    navigateToFeedback() {
+      this.$router.push('/feedback');
+    },
+    navigateToNotifications() {
+      this.$router.push('/notifications');
+    },
+    navigateToSupport() {
+      this.$router.push('/support');
+    },
+  },
+};
+</script>
+
+
