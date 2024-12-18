1ecc17052db2d2538907da74ade7b7ea0bd81389
S.Abolfazl Qasemzaadeh <100437667+S-Abolfazl@users.noreply.github.com>
2024-12-19 01:52:59 +0330

    starting ...

---
+++ Front/layouts/profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main class="h-max w-max">
+      <img src="image/profile/header.png" height="20%" width="100%" alt="img">
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'profile',
+  beforeMount() {
+    document.getElementById('loading').style.display = 'none';
+  },
+}
+</script>
---
+++ Front/pages/profile/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-row no-gutters>
+    <v-col cols="6" class="d-flex justify-center">
+      <v-row no-gutters class="justify-center">
+        <v-avatar color="primary" width="20%" height="100%" class="">
+          <img
+            v-if="user.avatar"
+            :src="user.avatar"
+            alt="user"
+          >
+          <v-icon v-else dark class="font_112">
+            mdi-account
+          </v-icon>
+        </v-avatar>
+
+        <div class="mr-7">
+          <b class="font_35">{{ user.first_name }}  {{ user.last_name }}</b>
+          <br>
+          <b class="font_30">{{ user.student_number }}</b>
+          <p class="font_26">{{ user.role }}</p>
+        </div>
+      </v-row>
+    </v-col>
+
+    <v-col cols="6" class="bg-success">
+
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  layout: "profile",
+  head(){
+    return {
+      title: 'پروفایل کاربری'
+    }
+  },
+  data: () => ({
+    user: {
+      avatar: "",
+      first_name: "حسین",
+      last_name: "گرزین",
+      student_number: "400243068",
+      role: "دانشجو"
+    }
+  }),
+  methods: {},
+}
+</script>
+<style scoped>
+p{
+  margin: 0 !important;
+}
+</style>
