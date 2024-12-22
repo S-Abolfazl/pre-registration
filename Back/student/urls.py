cce81ccbb2b91ed0ff5bf367a8d6fa3105378909
hgor1382 <hgor1382@gmail.com>
2024-12-22 11:35:51 +0330

    chart get api edited and no chart found handeled

--- Back/student/urls.py
+++ Back/student/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     # path('create/', StudentCreateApi.as_view(), name='create_student'),
     path('chart/create/', EducationalChartCreateApi.as_view(), name='create_chart'),
-    path('chart/get/', EducationalChartGetApi.as_view(), name='get_chart'),
+    path('chart/', EducationalChartGetApi.as_view(), name='get_chart'),
     path('selecet-passed-course/',AddCompletedCourseApi.as_view(), name='selecetpassed_Course'),
     path('courses/', CoursesForPassedCoursesApi.as_view(), name='get_courses'),
 ]
