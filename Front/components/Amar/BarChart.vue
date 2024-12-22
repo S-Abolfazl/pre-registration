8ffe19f75a75da5c2c76dd66a2c1e9cb8c51927b
S.Abolfazl Qasemzaadeh <100437667+S-Abolfazl@users.noreply.github.com>
2024-12-22 23:02:26 +0330

    implementing

--- Front/components/Amar/BarChart.vue
+++ Front/components/Amar/BarChart.vue
@@ -1,34 +1,90 @@
 <template>
   <div>
-    <apexchart type="bar" :options="chartOptions" :series="series" />
+    <apexchart type="bar" height="350" :options="chartOptions" :series="series" />
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      series: [
-        {
-          name: 'Course 1',
-          data: [50, 70, 90, 60, 80],
-        },
-        {
-          name: 'Course 2',
-          data: [40, 60, 80, 70, 90],
-        },
-      ],
-      chartOptions: {
-        chart: {
-          type: 'bar',
-          toolbar: { show: false },
-        },
-        xaxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-        },
-        colors: ['#FF5722', '#673AB7'],
+  data: () => ({
+    series: [{
+      name: 'Inflation',
+      data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+    }],
+    chartOptions: {
+      chart: {
+        height: 350,
+        type: 'bar',
       },
-    };
-  },
+      plotOptions: {
+        bar: {
+          borderRadius: 10,
+          dataLabels: {
+            position: 'top', // top, center, bottom
+          },
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function (val) {
+          return val + "%";
+        },
+        offsetY: -20,
+        style: {
+          fontSize: '12px',
+          colors: ["#304758"]
+        }
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        position: 'top',
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          fill: {
+            type: 'gradient',
+            gradient: {
+              colorFrom: '#D8E3F0',
+              colorTo: '#BED1E6',
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5,
+            }
+          }
+        },
+        tooltip: {
+          enabled: true,
+        }
+      },
+      yaxis: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false,
+        },
+        labels: {
+          show: false,
+          formatter: function (val) {
+            return val + "%";
+          }
+        }
+
+      },
+      title: {
+        text: 'Monthly Inflation in Argentina, 2002',
+        floating: true,
+        offsetY: 330,
+        align: 'center',
+        style: {
+          color: '#444'
+        }
+      }
+    },
+  }),
 };
 </script>
