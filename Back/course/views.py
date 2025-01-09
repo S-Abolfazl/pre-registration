900527130938994a54bc1318e3ac51ad6fd9d693
hgor1382 <hgor1382@gmail.com>
2025-01-09 19:27:30 +0330

    course update api edited to work with front

--- Back/course/views.py
+++ Back/course/views.py
@@ -135,50 +135,60 @@ class CourseUpdateApi(APIView):
     @swagger_auto_schema(
         operation_summary="Course Update",
         operation_description="Endpoint to update a course in term.",
-        request_body=CourseSerializer
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'courseName': openapi.Schema(type=openapi.TYPE_STRING, description='course name'),
+                'teacherName': openapi.Schema(type=openapi.TYPE_STRING, description='teacher name'),
+                'isExperimental': openapi.Schema(type=openapi.TYPE_BOOLEAN, description='is experimental'),
+                'class_time1': openapi.Schema(type=openapi.TYPE_STRING, description='class time1'),
+                'class_time2': openapi.Schema(type=openapi.TYPE_STRING, description='class time2'),
+                'class_start_time': openapi.Schema(type=openapi.TYPE_STRING, description='class start time'),
+                'class_end_time': openapi.Schema(type=openapi.TYPE_STRING, description='class end time'),
+                'exam_date': openapi.Schema(type=openapi.TYPE_STRING, description='exam date'),
+                'exam_start_time': openapi.Schema(type=openapi.TYPE_STRING, description='exam start time'),
+                'exam_end_time': openapi.Schema(type=openapi.TYPE_STRING, description='exam end time'),
+                'capacity': openapi.Schema(type=openapi.TYPE_INTEGER, description='capacity'),
+                'description': openapi.Schema(type=openapi.TYPE_STRING, description='description'),
+                'entry_years': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_STRING), description='entry years'),
+            }
+        )
     )
-    def put(self, request, pk):
-        try:
-            course = Course.objects.get(c_id=pk)
-            c = AllCourses.objects.get(course_id=request.data['course'])
-            request_data = request.data.copy()
-            request_data['course'] = c.course_id
-            serializer = CourseSerializer(course, data=request_data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(data={
-                    "msg":"ok",
-                    "data":f'درس با شناسه:{course.c_id} به روز شد',
-                    "status":status.HTTP_200_OK
-                }, status=status.HTTP_200_OK)
-            return Response(
-                data={
-                    "msg":"error",
-                    "data": serializer.errors,
-                    "satatus":status.HTTP_400_BAD_REQUEST
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        except:
-            return Response(data={
-                "msg":"error",
-                "data":"درس مورد نظر یافت نشد",
-            }, status=status.HTTP_404_NOT_FOUND)
-    
-    @swagger_auto_schema(
-        operation_summary="Course Update",
-        operation_description="Endpoint to update a course in term partialy.",
-        request_body=CourseSerializer
-    )        
     def patch(self, request, pk):
         try:
             course = Course.objects.get(c_id=pk)
-            c = AllCourses.objects.get(course_id=request.data['course'])
+            c = AllCourses.objects.get(courseName=request.data['courseName'])
             request_data = request.data.copy()
             request_data['course'] = c.course_id
-            serializer = CourseSerializer(course, data=request_data, partial=True)
+            request_data.pop('courseName', None)
+            if 'entry_years' in request_data:
+                request_data.pop('entry_years', None)
+            serializer = CourseSerializer(course, data=request_data)
             if serializer.is_valid():
                 serializer.save()
+                entry_years = request.data['entry_years']
+                if entry_years:
+                    entry_years = [year[-3:] if year[:2] == "14" else year[-2:] for year in entry_years]
+                    course_rule = CourseRule.objects.filter(course=course.c_id, type='entry_rule')
+                    if course_rule.exists():
+                        course_rule.update(values=entry_years)
+                    else:
+                        course_rule = CourseRuleSerializer(data={
+                            'course': course.c_id,
+                            'type': 'entry_rule',
+                            'values': entry_years,
+                        })
+                        if course_rule.is_valid():
+                            course_rule.save()
+                        else:
+                            return Response(data={
+                                "msg":"error",
+                                "data":course_rule.errors,
+                                "status":status.HTTP_400_BAD_REQUEST
+                            }, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    CourseRule.objects.filter(course=course.c_id, type='entry_rule').delete()
+                        
                 return Response(data={
                     "msg":"ok",
                     "data":f'درس با شناسه:{course.c_id} به روز شد',
@@ -192,10 +202,10 @@ class CourseUpdateApi(APIView):
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
-        except Course.DoesNotExist:
+        except Exception as e:
             return Response(data={
                 "msg":"error",
-                "data":"درس مورد نظر یافت نشد",
+                "data":"درس مورد نظر یافت نشد ",
             }, status=status.HTTP_404_NOT_FOUND)
         
 class CourseDeleteApi(APIView):
