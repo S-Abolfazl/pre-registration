1b8a0979491d3e98de75deb3d980233cbce76ec7
hgor1382 <hgor1382@gmail.com>
2025-01-15 18:40:47 +0330

    test for notification api

--- Back/notification/tests.py
+++ Back/notification/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from user.models import User
+from .models import Notification
+
+class NitificationGetApiTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="user",
+            password="password",
+            email="h@g.com",
+            type="student"
+        )
+        
+        self.notification = Notification.objects.create(
+            title="title",
+            content="content"
+        )
+        
+        self.client.force_authenticate(user=self.user)
+        
+    def test_get_notification_success(self):
+        response = self.client.get("/notification/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["msg"], "ok")
+        self.assertIn("data", response.data)
+        self.assertIsInstance(response.data["data"], list)
