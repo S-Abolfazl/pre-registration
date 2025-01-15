b090eddb6f439ff5c4721cb79918b5b97730b8ec
hgor1382 <hgor1382@gmail.com>
2025-01-15 18:22:34 +0330

    NotificationApi is ok now for receive all notification

--- Back/notification/views.py
+++ Back/notification/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Notification
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+class NotificationApi(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        operation_summary="Get all notifications",
+        operation_description="Get all notifications to show for user",
+    )
+    def get(self, request):
+        notifications = Notification.objects.all()
+        return Response(
+            data={
+                "msg" : "ok",
+                "data" : [
+                    {
+                        "title" : notification.title,
+                        "content" : notification.content,
+                        "created_at" : notification.created_at
+                    } for notification in notifications
+                ],
+                "status" : status.HTTP_200_OK
+            },
+            status=status.HTTP_200_OK
+        )
