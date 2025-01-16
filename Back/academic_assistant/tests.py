58f21fe29062ff8ff2d2596a18b0aff3a6788a85
hgor1382 <hgor1382@gmail.com>
2025-01-16 17:28:10 +0330

    test for participation percent is ok

--- Back/academic_assistant/tests.py
+++ Back/academic_assistant/tests.py
@@ -1,8 +1,9 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from user.models import User
+from user.serializers import UserSerializer
 from course.models import Course, AllCourses
-
+from registrationForm.models import RegistrationForm
 
 class TestBarChart(APITestCase):
     def setUp(self):
@@ -70,3 +71,94 @@ class TestOverflowedCourses(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["msg"], "ok")
         self.assertEqual(len(response.data["data"]), 1)
+
+class TestParticipationPercent(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test", email="h@g.com", password="test", type="academicassistant"
+        )
+        self.client.force_authenticate(user=self.user)
+        
+        test_users = [
+            {
+                "username": "400243020",
+                "password": "Hosein@1382",
+                "email": "student1@example.com",
+                "type": "student",
+            },
+            {
+                "username": "400234021",
+                "password": "Hosein@13  ",
+                "email": "student2@example.com",
+                "type": "student",
+            },
+            {
+                "username": "400243022",
+                "password": "Hosein@1382",
+                "email": "student3@example.com",
+                "type": "student",
+            },
+            {
+                "username": "400243023",
+                "password": "Hosein@1382",
+                "email": "student4@example.com",
+                "type": "student",
+            },
+            {
+                "username": "401243020",
+                "password": "Hosein@1382",
+                "email": "student8@example.com",
+                "type": "student",
+            },
+            {
+                "username": "402243020",
+                "password": "Hosein@1382",
+                "email": "student12@example.com",
+                "type": "student",
+            },
+            {
+                "username": "402234021",
+                "password": "Hosein@13  ",
+                "email": "student13@example.com",
+                "type": "student",
+            },
+            {
+                "username": "403243020",
+                "password": "Hosein@1382",
+                "email": "student16@example.com",
+                "type": "student",
+            },
+            {
+                "username": "403234021",
+                "password": "Hosein@13  ",
+                "email": "student17@example.com",
+                "type": "student",
+            },
+            {
+                "username": "99243020",
+                "password": "Hosein@1382",
+                "email": "student20@example.com",
+                "type": "student",
+            },
+            {
+                "username": "99234021",
+                "password": "Hosein@13  ",
+                "email": "student21@example.com",
+                "type": "student",
+            },
+        ]
+        
+        for user_data in test_users:
+            serializer = UserSerializer(data=user_data)
+            if serializer.is_valid():
+                user = serializer.save()
+
+                RegistrationForm.objects.create(student_id=user)
+                
+    def test_statistics_participation_percent_api_test(self):
+        response = self.client.get("/academic-assistant/statistics/participation-percent")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["msg"], "ok")
+        self.assertEqual(len(response.data["data"]), 5)
+        
+        
