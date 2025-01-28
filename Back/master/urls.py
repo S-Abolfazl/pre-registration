2765ca046e28e549830bfa65c351040ade4372ab
pouria-02 <p.adabi1381@gmail.com>
2025-01-28 10:26:06 +0330

    Mastelist url

--- Back/master/urls.py
+++ Back/master/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('create/', MasterCreateApi.as_view(), name = 'create_master'),
     path('delete/', MasterDeleteApi.as_view(), name = 'delete_master'),
     path('update/', MasterUpdateApi.as_view(), name = 'update_master'),
+    path('list/', MasterUpdateApi.as_view(), name = 'list_master'),
   #  path('create/', MasterCreateApi.as_view(), name = 'create_master'),
 ]
