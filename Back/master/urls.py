5e6eeefe745d18dfd06c0f01b9968d2447720794
pouria-02 <p.adabi1381@gmail.com>
2025-01-28 19:05:07 +0330

    t

--- Back/master/urls.py
+++ Back/master/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('create/', MasterCreateApi.as_view(), name = 'create_master'),
     path('delete/', MasterDeleteApi.as_view(), name = 'delete_master'),
     path('update/', MasterUpdateApi.as_view(), name = 'update_master'),
-  #  path('create/', MasterCreateApi.as_view(), name = 'create_master'),
+    path('list/', MasterListApi.as_view(), name = 'list_master'),
 ]
